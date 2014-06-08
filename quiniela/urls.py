26f9fd6e481afd6189a55c5d397346fbcb990fe2
leonardo <leonardogutierrezh@gmail.com>
2014-06-07 22:24:15 -0430

    banderas de la base de datos

--- quiniela/urls.py
+++ quiniela/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from quiniela import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'quiniela.views.home', name='home'),
     # url(r'^quiniela/', include('quiniela.foo.urls')),
-
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
