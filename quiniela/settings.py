c522c5d645d5599366299902aecae4cdbe2fa0a2
Francisco Gamundi <gamundiaranda@gmail.com>
2014-06-10 14:38:57 -0430

    COntext processors y authentication backends

--- quiniela/settings.py
+++ quiniela/settings.py
@@ -149,6 +149,25 @@ INSTALLED_APPS = (
     'allauth.socialaccount.providers.facebook',
 )
 
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+    'allauth.account.context_processors.account',
+    'allauth.socialaccount.context_processors.socialaccount',
+)
+
+AUTHENTICATION_BACKENDS = (
+    'allauth.account.auth_backends.AuthenticationBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
